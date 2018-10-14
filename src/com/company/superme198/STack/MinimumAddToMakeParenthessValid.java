2418f01a2143eaf1e1017347cb3e681300f67f4e
taidev198 <traigiuxom9x@gmail.com>
2018-10-14 11:15:40 +0700

    add minAddToMakeValid method

---
+++ src/com/company/superme198/STack/MinimumAddToMakeParenthessValid.java
@@ -0,0 +1,37 @@
+package com.company.superme198.STack;
+
+import java.util.Stack;
+
+public class MinimumAddToMakeParenthessValid {
+
+    /**Given a string S of '(' and ')' parentheses, we add the minimum number of parentheses ( '(' or ')', and in any positions )
+     * so that the resulting parentheses string is valid.
+     * Formally, a parentheses string is valid if and only if:
+     * It is the empty string, or
+     * It can be written as AB (A concatenated with B), where A and B are valid strings, or
+     * It can be written as (A), where A is a valid string.
+     * Given a parentheses string, return the minimum number of parentheses we must add to make the resulting string valid.*/
+    static int minAddToMakeValid(String S) {
+        int len = S.length();
+        if(len == 0) return 0;
+        int min = 0;
+        Stack<Character> stack = new Stack<>();
+        char[] charArray = S.toCharArray();
+        for(int i=0; i< len; i++){
+            if(charArray[i] == '(')
+                stack.push(charArray[i]);
+            else {
+                if(stack.isEmpty())
+                    min++;
+                else stack.pop();
+            }
+        }
+        return min + stack.size();
+
+    }
+
+    public static void main(String...args){
+        System.out.println(minAddToMakeValid("()))(("));
+
+    }
+}
