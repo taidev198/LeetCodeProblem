6c877d2883d140f0d2084737c9298bd14020fc1b
taidev198 <traigiuxom9x@gmail.com>
2018-10-28 19:02:30 +0700

    add solution 2 using binary search

--- src/com/company/superme198/BinarySearch/IsSequence.java
+++ src/com/company/superme198/BinarySearch/IsSequence.java
@@ -1,5 +1,9 @@
 package com.company.superme198.BinarySearch;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class IsSequence {
 
     /**Link:https://leetcode.com/problems/is-subsequence/description/
@@ -37,5 +41,36 @@ public class IsSequence {
         }
         return false;
 
+    }
+
+    /**Solution 2:*/
+    static // Follow-up: O(N) time for pre-processing, O(Mlog?) for each S.
+    // Eg-1. s="abc", t="bahbgdca"
+    // idx=[a={1,7}, b={0,3}, c={6}]
+    //  i=0 ('a'): prev=1
+    //  i=1 ('b'): prev=3
+    //  i=2 ('c'): prev=6 (return true)
+    // Eg-2. s="abc", t="bahgdcb"
+    // idx=[a={1}, b={0,6}, c={5}]
+    //  i=0 ('a'): prev=1
+    //  i=1 ('b'): prev=6
+    //  i=2 ('c'): prev=? (return false)
+    public boolean isSubsequence1(String s, String t) {
+        List<Integer>[] idx = new List[256]; // Just for clarity
+        for (int i = 0; i < t.length(); i++) {
+            if (idx[t.charAt(i)] == null)
+                idx[t.charAt(i)] = new ArrayList<>();
+            idx[t.charAt(i)].add(i);
+        }
+
+        int prev = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (idx[s.charAt(i)] == null) return false; // Note: char of S does NOT exist in T causing NPE
+            int j = Collections.binarySearch(idx[s.charAt(i)], prev);
+            if (j < 0) j = -j - 1;
+            if (j == idx[s.charAt(i)].size()) return false;
+            prev = idx[s.charAt(i)].get(j) + 1;
+        }
+        return true;
     }
 }
