b937058aa1370919b8509ae80292009bdbe964eb
taidev198 <traigiuxom9x@gmail.com>
2018-11-08 07:55:21 +0700

    Binary Tree Tilt Using DFS

---
+++ src/com/company/superme198/Tree/BinaryTreeTilt.java
@@ -0,0 +1,47 @@
+package com.company.superme198.Tree;
+
+import com.company.superme198.TreeNode;
+
+public class BinaryTreeTilt {
+
+    /**Link:https://leetcode.com/problems/binary-tree-tilt/description/
+     * Given a binary tree, return the tilt of the whole tree.
+     *
+     * The tilt of a tree node is defined as the absolute difference between
+     * the sum of all left subtree node values and the sum of all right subtree node values. Null node has tilt 0.
+     *
+     * The tilt of the whole tree is defined as the sum of all nodes' tilt.
+     *
+     * Example:
+     * Input:
+     *          1
+     *        /   \
+     *       2     3
+     * Output: 1
+     * Explanation:
+     * Tilt of node 2 : 0
+     * Tilt of node 3 : 0
+     * Tilt of node 1 : |2-3| = 1
+     * Tilt of binary tree : 0 + 0 + 1 = 1
+     * Note:
+     *
+     * The sum of node values in any subtree won't exceed the range of 32-bit integer.
+     * All the tilt values won't exceed the range of 32-bit integer.*/
+
+    static  int findTilt(TreeNode root) {
+        if(root == null)
+            return 0;
+        int left = sum(root.left);
+        int right = sum(root.right);
+
+        return Math.abs(left - right) +findTilt(root.left) +
+                findTilt(root.right);
+    }
+
+    static int sum(TreeNode root){
+
+        if(root == null)
+            return 0;
+        return root.val + sum(root.left) + sum(root.right);
+    }
+}
