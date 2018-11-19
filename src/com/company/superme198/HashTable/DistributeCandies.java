d441ca97fe273480659bd0863740ad7a1948b818
taidev198 <traigiuxom9x@gmail.com>
2018-11-19 17:59:07 +0700

    575. Distribute Candies

---
+++ src/com/company/superme198/HashTable/DistributeCandies.java
@@ -0,0 +1,56 @@
+package com.company.superme198.HashTable;
+
+import java.util.*;
+
+/**
+ * Created by traig on 8:53 AM, 11/19/2018
+ */
+public class DistributeCandies {
+
+    /**Link:https://leetcode.com/problems/distribute-candies/
+     * Given an integer array with even length, where different numbers in this array represent different kinds of candies.
+     * Each number means one candy of the corresponding kind. You need to distribute these candies equally in number to brother and sister.
+     * Return the maximum number of kinds of candies the sister could gain.
+     * Example 1:
+     * Input: candies = [1,1,2,2,3,3]
+     * Output: 3
+     * Explanation:
+     * There are three different kinds of candies (1, 2 and 3), and two candies for each kind.
+     * Optimal distribution: The sister has candies [1,2,3] and the brother has candies [1,2,3], too.
+     * The sister has three different kinds of candies.
+     * Example 2:
+     * Input: candies = [1,1,2,3]
+     * Output: 2
+     * Explanation: For example, the sister has candies [2,3] and the brother has candies [1,1].
+     * The sister has two different kinds of candies, the brother has only one kind of candies. */
+    static int distributeCandies(int[] candies) {
+        int len = candies.length;
+        Hashtable<Integer, Integer> res = new Hashtable<>();
+        int ans = 0;
+        for (int i = 0; i < len; i++) {
+            if (!res.containsKey(candies[i]))
+                res.put(candies[i], 1);
+            else {
+                int value =  res.get(candies[i]);
+                res.replace(candies[i], value, value+1);
+            }
+        }
+        List<Integer> tmp = new ArrayList<>(res.values());
+        if(tmp.size() <= len /2)
+            return tmp.size();
+        int temp = len/2;
+        for (int i = 0; temp != 0; i++) {
+            i = i % tmp.size();
+            if (tmp.get(i) > 0){
+                temp --;
+                tmp.set(i, tmp.get(i) -1);
+                ans++;
+            }
+        }
+        return ans;
+    }
+
+    public static void main(String...args){
+        System.out.println(distributeCandies(new int[]{1,1, 2,3, 2,2}));
+    }
+}
