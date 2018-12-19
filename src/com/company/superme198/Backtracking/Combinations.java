b7655a29af0969b058c47f5676fd48aec153efbc
taidev198 <traigiuxom9x@gmail.com>
2018-12-19 11:27:00 +0700

    77. Combinations

--- src/com/company/superme198/Backtracking/Combinations.java
+++ src/com/company/superme198/Backtracking/Combinations.java
@@ -6,7 +6,21 @@ import java.util.List;
 public class Combinations {
 
 
-
+    /**Link:https://leetcode.com/problems/combinations/
+     * Given two integers n and k, return all possible combinations of k numbers out of 1 ... n.
+     *
+     * Example:
+     *
+     * Input: n = 4, k = 2
+     * Output:
+     * [
+     *   [2,4],
+     *   [3,4],
+     *   [2,3],
+     *   [1,2],
+     *   [1,3],
+     *   [1,4],
+     * ]*/
     static List<List<Integer>> combine(int n, int k) {
 
         boolean[] visited = new boolean[n+1];
@@ -18,21 +32,15 @@ public class Combinations {
 
     static void combineHelper(int n, int k, List<List<Integer>> ans, List<Integer> chosen, boolean[] visited){
         if(chosen.size() == k){
-          //  if(!ans.contains(chosen) )
-                ans.add(new ArrayList(chosen));
+            ans.add(new ArrayList(chosen));
         }else{
             for(int i =1; i <= n ;i++){
-                if(!visited[i]){
+                if(chosen.size() ==0  || chosen.get(chosen.size()-1 ) < i){
                     visited[i] = true;
-                    if(chosen.size() ==0  || chosen.get(chosen.size()-1 ) < i){
-                        chosen.add(i);
-                    }
+                    chosen.add(i);
                     combineHelper(n, k, ans, chosen, visited);
                     visited[i] = false;
-                    if (chosen.size() > 0){
-                        chosen.remove(chosen.size() -1);
-                    }
-
+                    chosen.remove(chosen.size() -1);
 
                 }
             }
